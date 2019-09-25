0af015422b904ea438196f36b79fda707e5cd997
Léo Metge <metge.leo@gmail.com>
2019-09-25 14:22:44 +0200

    first commit

---
+++ src/main/java/lpmms/lmet/App.java
@@ -0,0 +1,15 @@
+package lpmms.lmet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{
+    /**
+     * methode principal qui dit "Hello Word!"
+     * @param args notuse
+     */
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
